6543925866310e815d3c53179766eff469460f09
Chandan <hyeiamcosmos@gmail.com>
2023-05-20 22:12:03 +0530

    change height

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 const GameCardSkeleton = () => {
   return (
     <Card width='270px' borderRadius={10} overflow="hidden">
-        <Skeleton height="200px"/>
+        <Skeleton height="210px"/>
         <CardBody>
             <SkeletonText/>
         </CardBody> 
