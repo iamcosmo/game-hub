e510263a2a544b1d7e3b5f5f62984521b473615b
Chandan <hyeiamcosmos@gmail.com>
2023-05-23 00:24:24 +0530

    other ways...

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -5,13 +5,25 @@ interface Props {
   gameQuery: GameQuery | null;
 }
 const GameHeading = ({ gameQuery }: Props) => {
+    //Other Way...
+
+    //const heading = `${gameQuery?.platform?.name|| ''} ${gameQuery?.genre?.name|| ''} Games`;
   return (
+    //Other Way.....
+
+    /*<Heading as="h1" paddingBottom={5} fontSize='5xl'>
+            {heading}
+      </Heading>*/
+
+
+      //My Way
     <Heading as="h1" paddingBottom={5} fontSize='5xl'>
       {(gameQuery?.platform?.name || "") +
         "\t" +
         (gameQuery?.genre?.name || "") +
         "\tGames"}
     </Heading>
+    //My Way
   );
 };
 
