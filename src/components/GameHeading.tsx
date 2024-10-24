edbfb53f521810b6758b317b63999d876f094c22
Chandan <hyeiamcosmos@gmail.com>
2024-10-24 12:43:51 +0530

    create lookup hooks

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,18 +1,16 @@
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
-import useGenres from "../hooks/useGenres";
-import usePlatforms from './../hooks/usePlatforms';
+import useGenre from "../hooks/useGenre";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   gameQuery: GameQuery | null;
 }
 const GameHeading = ({ gameQuery }: Props) => {
 
-  const {data:genres} = useGenres();
-  const genre = genres?.results.find(g=>g.id===gameQuery?.genreId)
+  const genre = useGenre(gameQuery?.genreId)
 
-  const { data:Platforms} = usePlatforms();
-  const platform = Platforms?.results.find(p=>p.id===gameQuery?.platformId)
+  const platform = usePlatform(gameQuery?.platformId)
    
   return (
    
