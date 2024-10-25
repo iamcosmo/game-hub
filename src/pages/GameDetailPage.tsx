a1bc992900ef5165af346a3b2c705ec92f515923
Chandan <hyeiamcosmos@gmail.com>
2024-10-25 12:27:04 +0530

    Skeleton added instead of spinner

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,13 +1,21 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Skeleton, Spinner, Text } from "@chakra-ui/react";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
 
-  if(isLoading) return <Spinner/>
+  if (isLoading) {
+    return (
+      <>
+        <Skeleton height="40px" />
+        <Skeleton height="20px" mt="4" />
+      </>
+    );
+  }
+  
   if (error || !game) throw error;
 
   return (
