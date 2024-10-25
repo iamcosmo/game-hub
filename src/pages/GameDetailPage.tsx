fc972ef02ef9c5fa0f3d1c022a5d4729e43ba1df
Chandan <hyeiamcosmos@gmail.com>
2024-10-25 17:33:23 +0530

    Screenshots added

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailers from "../components/GameTrailers";
 import useGame from "../hooks/useGame";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -30,6 +31,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailers gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
