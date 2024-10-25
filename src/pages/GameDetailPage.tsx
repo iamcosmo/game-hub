b971b56baeb0b1ca182b3821d40a42acac8e629f
Chandan <hyeiamcosmos@gmail.com>
2024-10-25 13:15:25 +0530

    Game Details Page added with Attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,12 +1,12 @@
 import { Heading, Skeleton } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
-
 
   if (isLoading) {
     return (
@@ -18,13 +18,16 @@ const GameDetailPage = () => {
       </>
     );
   }
-  
+
   if (error || !game) throw error;
 
   return (
     <>
-      <Heading size='2xl' marginBottom={4}>{game.name}</Heading>
-      <ExpandableText >{game.description_raw}</ExpandableText>
+      <Heading size="2xl" marginBottom={4}>
+        {game.name}
+      </Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
